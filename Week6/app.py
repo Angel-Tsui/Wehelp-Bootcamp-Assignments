9f710184138b4b5bde0484c8252e7edbae624488
Angel-Tsui <131181098+Angel-Tsui@users.noreply.github.com>
2023-08-20 00:19:07 +0800

    Update app.py

--- Week6/app.py
+++ Week6/app.py
@@ -3,7 +3,6 @@ from flask import request
 from flask import redirect
 from flask import render_template
 from flask import session
-from flask import url_for
 import mysql.connector
 import json
 con=mysql.connector.connect(
